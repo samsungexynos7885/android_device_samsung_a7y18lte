fd3a7aeebb3fcce11dbadd6b8b7a090df4a47ee8
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-09-28 14:14:41 +0800

    a7y18lte : pbrp bringup

--- omni_a7y18lte.mk
+++ omni_a7y18lte.mk
@@ -4,8 +4,8 @@ PRODUCT_RELEASE_NAME := a7y18lte
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit common product files.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 PRODUCT_PACKAGES += \
 	charger_res_images \
